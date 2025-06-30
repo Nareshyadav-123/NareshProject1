abe2093085f43807cc88a2c35b3d0a264a9485a3
Nareshyadav-123 <nareshyadav72386@gmail.com>
2025-06-30 13:28:48 +0530

    "added changes" (#1)

---
+++ component1/src/loginpgae/loginpgae.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginpgae',
+  templateUrl: './loginpgae.component.html',
+  styleUrls: ['./loginpgae.component.scss']
+})
+export class LoginpgaeComponent {
+
+}
